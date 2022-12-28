0f13b8deb9ceced00fe6aef50dccec5554ae28e5
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-12-27 21:37:11 -0800

    Update redrawn.bat

--- redrawn.bat
+++ redrawn.bat
@@ -1,5 +1,11 @@
 @echo off
 if not exist %USERPROFILE%\Redrawn echo Redrawn Offline Does Not Exist On Your System. && pause & exit
+if not exist %USERPROFILE%\Redrawn-Express (
+        echo Redrawn Express is required in order to boot Redrawn Offline from here. 
+        echo Redrawn Offline has been detected on your system so you can boot it from the dualboot.bat file.
+        pause 
+        exit
+)
 cd %USERPROFILE%\Redrawn
 echo Starting Redrawn Offline...
 call Redrawn.exe
